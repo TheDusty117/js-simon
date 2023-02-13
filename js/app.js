eee422e3af8fde709ea7d891bb384fc285cd1a80
TheDusty117 <gabrieledipassio@gmail.com>
2023-02-13 15:34:22 +0100

    FEAT random 5 nums in alert

---
+++ js/app.js
@@ -0,0 +1,34 @@
+//VISUALIZZARE 5 NUMERI RANDOM CON ALERT
+
+const fiveRandomNums = [] 
+  
+  
+  for (let i = 0; i < 5; i++) {
+    const numRandom = Math.floor(Math.random(1)*100+1)
+    fiveRandomNums.push(numRandom)
+  }
+  console.log(fiveRandomNums)
+
+  let fiveRandomsAlert = alert ( fiveRandomNums )
+
+
+
+
+//DOPO I 30 SECONDI UTENTE DEVE INSERIRE TRAMITE PROMPT GLI STESSI NUMERI RANDOM DELL'ALERT
+
+
+
+//CONTROLLARE SE I 5 NUMERI PROMPT inseriti da utente SONO UGUALI AI 5 dell'ALERT
+
+
+
+
+//STAMPARE QUANTI NUMERI SONO STATI INSERITI CORRETTAMENTE
+
+
+
+
+
+
+
+
